f6a03b3b9b1946d5a473f1f9177063ead5d5250e
Andrew Grass <andygrass92@gmail.com>
2019-04-25 16:28:39 -0400

    Updates

---
+++ precache-manifest.ee82a2b26b0716402044f52364b9387c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dda4b71a40f84457c449",
+    "url": "/TheAlmightyStringReverser/static/js/runtime~main.1f8d5b28.js"
+  },
+  {
+    "revision": "b3514384eead4c2b1159",
+    "url": "/TheAlmightyStringReverser/static/js/main.cbc43aef.chunk.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/TheAlmightyStringReverser/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "b3514384eead4c2b1159",
+    "url": "/TheAlmightyStringReverser/static/css/main.78a9badc.chunk.css"
+  },
+  {
+    "revision": "3d8f04a13b4e21e926e0828ef9561aab",
+    "url": "/TheAlmightyStringReverser/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/thealmightystringreverser/precache-manifest.a4f083c760395446b7f07ea9c577909d.js"
+  "/TheAlmightyStringReverser/precache-manifest.ee82a2b26b0716402044f52364b9387c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/thealmightystringreverser/index.html", {
+workbox.routing.registerNavigationRoute("/TheAlmightyStringReverser/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
