e368508b63782dffc5938b4fb9fc3f8e87488e8e
Andrew Grass <andygrass92@gmail.com>
2019-04-25 15:48:48 -0400

    removed unnecessary apps from index.js

--- src/index.js
+++ src/index.js
@@ -1,45 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
-/****************
-*   JOKE APP    *
-*****************/
-// import JokeApp from './apps/JokeApp';
-// ReactDOM.render(<JokeApp />, document.getElementById("root"))
-
-/********************
- *  PRODUCTS APP    *
- ********************/
-// import ProductApp from './apps/Products/ProductApp';
-// ReactDOM.render(<ProductApp />, document.getElementById("root"));
-
-/********************
- *  STATE PRACTICE  *
- ********************/
-// import  StateApp from './apps/State/StateApp';
-// ReactDOM.render(<StateApp />, document.getElementById("root"));
-
-/*********************
- *  STRING REVERSAL  *
- *********************/
-import ReversalApp from './apps/StringReversal/ReversalApp';
-ReactDOM.render(<ReversalApp />, document.getElementById("root"));
-
-/****************
- *  CALCULATOR  *
- ****************/
-// import CalculatorApp from './apps/Calculator/CalculatorApp';
-// ReactDOM.render(<CalculatorApp />, document.getElementById("root"));
-/*
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReversalApp from './apps/StringReversal/ReversalApp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/************************
+*  STRING REVERSAL APP  *
+*************************/
+ReactDOM.render(<ReversalApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-*/
+
