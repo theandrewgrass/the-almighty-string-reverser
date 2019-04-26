1b7f9e68299d3872e6a6422666be3e569482500f
Andrew Grass <andygrass92@gmail.com>
2019-04-26 13:55:44 -0400

    added media queries and url to dash-delimited

--- src/apps/StringReversal/components/UserInput.js
+++ src/apps/StringReversal/components/UserInput.js
@@ -4,7 +4,7 @@ import React from 'react';
 function UserInput(props) {
     return (
         <div className="input-section">
-            <h1 className="description-text">The Almighty String Reverser</h1>
+            <h1 className="description-text">The<br/><span className="almighty-text">Almighty</span><br/>String Reverser</h1>
             <input className="text-box" placeholder="(Type your string here!)" type="text" onChange={props.getString} />
         </div>
     );
